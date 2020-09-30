253db8a2e9ab90174dd0080d13e3553f67dd60ec
Sai Meghana <saimeghanagudisa@gmail.com>
2020-09-30 15:54:10 +0530

    add and delete vehicles functionalities working

--- src/main/java/com/example/vehicles/controller/VehiclesController.java
+++ src/main/java/com/example/vehicles/controller/VehiclesController.java
@@ -1,10 +1,14 @@
 package com.example.vehicles.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,25 +19,86 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.vehicles.Repository.VehiclesRepository;
 import com.example.vehicles.model.Vehicle;
+import com.fasterxml.jackson.annotation.JsonValue;
 
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 @RequestMapping("/vehicle")
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
+//@RequestMapping(value="/vehicle", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class VehiclesController {
 	
 	@Autowired
 	private VehiclesRepository service;
+	int count=0;
+	//List <Vehicle> vehicles=new ArrayList<Vehicle>();
+	//private ArrayList[] objects;
+	//private List<Vehicle> vehicles;
 	
-	
-	@PostMapping(value="/addvehicle",consumes = "application/json", produces = "application/json")
-	public void addVehicle(@RequestBody Vehicle vehicle) {
-		service.save(vehicle);	
-		//return "Employee(s) added!";
+	//@PostMapping(value="/addvehicle",consumes = "application/json", produces = "application/json")
+	//public void addVehicle(@RequestBody Object obj) {
+		// List list = java.util.Arrays.asList(obj.toString());
+		// for(int i=0;i<list.size();i++)
+		// 	service.save(list.get(i));
+		// for(Vehicle veh:obj){
+		// 	vehicles.add(veh);
+		// }
+
+		
+		//@RequestMapping(method=RequestMethod.POST,value="/addvehicle",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//		@RequestMapping(method=RequestMethod.POST,value="/addvehicle", consumes = MediaType.APPLICATION_JSON_VALUE)
+//	@RequestMapping(method=RequestMethod.POST,value="/addvehicle",consumes="application/json")
+//		public void addVehicle(@RequestBody JsonObject obj)
+//		{
+//			for(int i=0;i<obj.size();i++)
+//			{
+//				JsonArray array= (JsonArray) obj.get(i);
+//				//service.saveAll(array);
+//				System.out.print(array);
+//			}
+//			for(int i=0;i<arr.size();i++)
+//			{
+//				Vehicle veh=(Vehicle) arr.get(i);
+//				service.save(veh);
+//			}
+			//for(Vehicle veh: vehicle)
+				//service.save(veh);
+			//List<String> response = new ArrayList<String>();
+			//System.out.println(vehicle);
+//			for(int i=0;i<jsonObject.length;i++)
+//			{
+//				JsonArray array= jsonObject.get(i)
+//			}
+//			JsonArray array= jsonObject.getJsonArray("tableRows");
+//			for(javax.json.JsonValue val: array)
+//			{
+//				service.save((Vehicle) val);
+//			}
+		//}
+	@PostMapping(value="/addvehicle")
+	public void addVehicle(@RequestBody Vehicle vehicle)
+	{
+		if(count<3)
+		{
+		service.save(vehicle);
+		count++;
+		}
 	}
-	
+		
 	@PutMapping(value="/updatevehicle")
 	public void updateVehicle(@RequestBody Vehicle vehicle) {
 		service.save(vehicle);
+	}
+
+	//@DeleteMapping(value = "/deletevehicle")
+	
+	@DeleteMapping(value="/getvehicles/{id}")
+	public void deleteVehicle(@PathVariable String id){
+		service.deleteById(id);
+		count--;
 	}
 	
 	//@RequestMapping(method=RequestMethod.GET,value="/getvehicles",produces = "application/json")
@@ -42,11 +107,4 @@ public class VehiclesController {
 	public List<Vehicle> getVehicles(){
 		return (List<Vehicle>) service.findAll();
 	}
-	
-	
-	
-	
-	
-	
-	
 }
--- src/main/java/com/example/vehicles/model/Vehicle.java
+++ src/main/java/com/example/vehicles/model/Vehicle.java
@@ -1,28 +1,30 @@
 package com.example.vehicles.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-
 @Entity
 @Table(name="Vehicle")
-
 public class Vehicle {
+
 
 	@Id
 	private String registration_no;  //Primary Key
 	private int resident_id; // Foreign Key
 	private int vehicle_type;
-	
+	private String vehicle_make;
+	//private boolean is_editable;
 	public Vehicle() {}
 	
-	public Vehicle(String regNo, int resId, int vehType) {
+	public Vehicle(String regNo, String resId, String vehType,String vehMake) {
 		super();
 		this.registration_no = regNo;
-		this.resident_id = resId;
-		this.vehicle_type = vehType;
+		this.resident_id = Integer.parseInt(resId);
+		this.vehicle_type = Integer.parseInt(vehType);
+		this.vehicle_make = vehMake;
+		//this.is_editable = isEditable;
 	}
 
 	public String getRegistrationNo() {
@@ -48,9 +50,20 @@ public class Vehicle {
 	public void setVehicleType(int vehType) {
 		this.vehicle_type = vehType;
 	}
-	
-	
-	
-	
-	
+
+	public String getVehicleMake() {
+		return vehicle_make;
+	}
+
+	public void setVehicleMake(String vehmodel) {
+		this.vehicle_make = vehmodel;
+	}
+
+//	public boolean getIs_editable() {
+//		return is_editable;
+//	}
+//
+//	public void setIs_editable(boolean is_editable) {
+//		this.is_editable = is_editable;
+//	}	
 }
